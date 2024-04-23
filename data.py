af7305c3611a6896dba5ee0403947fdff27f7361
Praval <p.singhx26@gmail.com>
2024-04-23 21:48:47 +0530

    render temperature data on page

--- data.py
+++ data.py
@@ -6,9 +6,18 @@ def get_data(place, forecast_days=None, data_type=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     request = requests.get(url)
     data = request.json()
-    return data
+    filteredData = data['list']
+    nrValue = forecast_days * 8
+    filteredData = filteredData[:nrValue]
+    temperatures = []
+    dates = []
+    if data_type == 'Temperature':
+        temperatures = [filteredData[i]['main']['temp']/10 for i in range(len(filteredData))]
+        dates = [filteredData[i]['dt_txt'] for i in range(len(filteredData))]
+    return dates, temperatures
 
 
 if __name__ == "__main__":
-    data = get_data('Jammu')
-    print(data['list'][0]['main']['temp']/10)
+    data = get_data('Jammu', 3, 'Temperature')
+    print(data)
+
--- home.py
+++ home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from data import get_data
 
 st.title("Weather Forecast for the next Days")
 place = st.text_input("Place: ")
@@ -7,13 +8,6 @@ days = st.slider("Forecast Days", 1, 5, help="Select the number of forecasted da
 dataType = st.selectbox("Select Data to View", ['Temperature', 'Sky'])
 st.subheader(f"{dataType} for the next {days} days in {place}")
 
-
-def get_data(days):
-    dates = ['2024-10-01', '2024-10-02', '2024-10-03']
-    temperatures = [10, 9, 7]
-    return dates, temperatures
-
-
-d, t = get_data(days)
+d, t = get_data(place, days, dataType)
 figure = px.line(x=d, y=t, labels={'x': 'Date', 'y': 'Temperature (C)'})
 st.plotly_chart(figure)
